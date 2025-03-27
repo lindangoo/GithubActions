a50c06423b2f842d34298e9a75c69caae233bc55
linda ngo <lindango@lindas-MBP.home.local>
2025-03-27 00:13:47 -0500

    updated names and implement other unit tests

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -4,7 +4,7 @@ namespace GithubActionsLab;
 public class Addition
 {
 	[TestMethod]
-	public void Add_Valid_Patino()
+	public void Add_Valid_Ngo()
 	{
 		Assert.AreEqual(3, Program.Add("1", "2"));
 		Assert.AreEqual(5, Program.Add("3", "2"));
@@ -12,7 +12,7 @@ public class Addition
 	}
 
 	[TestMethod]
-	public void Add_Invalid_Patino()
+	public void Add_Invalid_Ngo()
 	{
 		Assert.ThrowsException<FormatException>(() => Program.Add("1", "a"));
 		Assert.ThrowsException<FormatException>(() => Program.Add("a", "1"));
@@ -20,10 +20,122 @@ public class Addition
 	}
 
 	[TestMethod]
-	public void Add_Null_Patino()
+	public void Add_Null_Ngo()
 	{
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add("1", null));
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, "1"));
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, null));
+	}
+}
+
+[TestClass]
+public class Subtraction
+{
+	[TestMethod]
+	public void Subtract_Valid_Ngo()
+	{
+		Assert.AreEqual(2, Program.Subtract("4", "2"));
+		Assert.AreEqual(0, Program.Subtract("1", "1"));
+		Assert.AreEqual(-3, Program.Subtract("5", "8"));
+	}
+
+	[TestMethod]
+	public void Subtract_Invalid_Ngo()
+	{
+		Assert.ThrowsException<FormatException>(() => Program.Subtract("1", "a"));
+		Assert.ThrowsException<FormatException>(() => Program.Subtract("a", "1"));
+		Assert.ThrowsException<FormatException>(() => Program.Subtract("a", "a"));
+	}
+
+	[TestMethod]
+	public void Subtract_Null_Ngo()
+	{
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract("1", null));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract(null, "1"));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract(null, null));
+	}
+}
+
+[TestClass]
+public class Multiplication
+{
+	[TestMethod]
+	public void Multiply_Valid_Ngo()
+	{
+		Assert.AreEqual(6, Program.Multiply("2", "3"));
+		Assert.AreEqual(15, Program.Multiply("3", "5"));
+		Assert.AreEqual(30, Program.Multiply("6", "5"));
+	}
+
+	[TestMethod]
+	{
+		Assert.ThrowsException<FormatException>(() => Program.Multiply("1", "a"));
+		Assert.ThrowsException<FormatException>(() => Program.Multiply("a", "1"));
+		Assert.ThrowsException<FormatException>(() => Program.Multiply("a", "a"));
+	}
+
+	[TestMethod]
+	public void Multiply_Null_Ngo()
+	{
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply("1", null));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply(null, "1"));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply(null, null));
+	}
+}
+
+[TestClass]
+public class Division
+{
+	[TestMethod]
+	public void Divide_Valid_Ngo()
+	{
+		Assert.AreEqual(1.5, Program.Divide("3", "2"));
+		Assert.AreEqual(3, Program.Divide("9", "3"));
+		Assert.AreEqual(12, Program.Divide("60", "5"));
+	}
+
+	[TestMethod]
+	public void Divide_Invalid_Ngo()
+	{
+		Assert.ThrowsException<FormatException>(() => Program.Divide("1", "a"));
+		Assert.ThrowsException<FormatException>(() => Program.Divide("a", "1"));
+		Assert.ThrowsException<FormatException>(() => Program.Divide("a", "a"));
+	}
+
+	[TestMethod]
+	public void Divide_Null_Ngo()
+	{
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Divide("1", null));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Divide(null, "1"));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Divide(null, null));
+	}
+}
+
+[TestClass]
+public class Power
+{
+	[TestMethod]
+	public void Power_Valid_Ngo()
+	{
+		// Intentionally failing test - 
+		Assert.AreEqual(8, Program.Power("2", "3"));
+		Assert.AreEqual(25, Program.Power("5", "2"));
+		Assert.AreEqual(729, Program.Power("9", "3"));
+	}
+
+	[TestMethod]
+	public void Power_Invalid_Ngo()
+	{
+		Assert.ThrowsException<FormatException>(() => Program.Power("1", "a"));
+		Assert.ThrowsException<FormatException>(() => Program.Power("a", "1"));
+		Assert.ThrowsException<FormatException>(() => Program.Power("a", "a"));
+	}
+
+	[TestMethod]
+	public void Power_Null_Ngo()
+	{
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Power("1", null));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null, "1"));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null, null));
 	}
 }
